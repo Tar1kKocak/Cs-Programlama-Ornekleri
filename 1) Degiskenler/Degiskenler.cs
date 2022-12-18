52fb464bb4c4d4e3908e2173d46bfc53be1dd7e7
Tarık <116958073+Tar1kKocak@users.noreply.github.com>
2022-12-18 10:41:34 +0300

    Add files via upload

---
+++ 1) Degiskenler/Degiskenler.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Degiskenler
+{
+    internal class Program
+    {
+
+        static void Main(string[] args)
+        {
+            char karakter = 'T';
+            string isim = "Tarik";
+            int tamsayi = 5;
+            float kesirlisayi = 1.4f;
+            double buyukkesirlisayi = 9.4;
+            bool anahtar = true;
+
+            Console.Write(karakter + "\n");
+            Console.Write(isim + "\n");
+            Console.Write(tamsayi + kesirlisayi + "\n");
+            Console.Write(buyukkesirlisayi + "\n");
+
+            Console.Write(karakter);
+            Console.Write(isim);
+            Console.WriteLine(tamsayi + kesirlisayi);
+            Console.WriteLine(buyukkesirlisayi);
+
+            Console.WriteLine("sayi = {0},isim = {1}", kesirlisayi, isim);
+
+            Console.WriteLine("sayi = " + kesirlisayi + ",isim =  " + isim);
+
+
+
+            int kesirlisayininboyut = Marshal.SizeOf(kesirlisayi);
+            Console.WriteLine(kesirlisayininboyut);
+
+        }
+    }
+}
