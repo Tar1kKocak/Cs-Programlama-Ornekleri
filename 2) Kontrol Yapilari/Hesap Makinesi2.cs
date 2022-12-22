e4a25454d31c02cdf3c74e0dc3e71f1e7cd6da59
Tarık <116958073+Tar1kKocak@users.noreply.github.com>
2022-12-22 22:27:52 +0300

    Add files via upload

---
+++ 2) Kontrol Yapilari/Hesap Makinesi2.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _8
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // Yazilimin Amaci : Hesap Makinesi 
+
+            char formul;
+            float sayi1, sayi2;
+
+            Console.WriteLine("Lütfen yapmak istediginiz islemin sembolunu seciniz.\n");
+            Console.WriteLine("1-Toplama (+)\n2-Çikarma(-)\n3-Carpma(*)\n4-Bolme(/)\n");
+            formul=Console.ReadKey().KeyChar;
+            Console.WriteLine("\n");
+            Console.WriteLine("Lutfen iki adet sayi giriniz");
+            sayi1 = int.Parse(Console.ReadLine());
+            sayi2 = int.Parse(Console.ReadLine());
+
+           switch(formul)
+            {
+                case '+':Console.WriteLine("Toplam:"+(sayi1 + sayi2)); break;
+                case '-':Console.WriteLine("Cikartma:"+(sayi1- sayi2)); break;
+                case '*':Console.WriteLine("Carpma:"+(sayi1 * sayi2)); break;
+                case '/':Console.WriteLine("Bolme:"+(sayi1/ sayi2)); break;
+            }
+        }
+    }
+}
---
+++ 2) Kontrol Yapilari/Meslek-Maas.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace sc
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //Yazilimin Amaci : Meslek-Maas Sorgulama.
+
+            int mesleksecenek;
+
+            Console.WriteLine("Maasini ogrenmek istediginiz meslegi seciniz");
+         a:
+            Console.WriteLine("1-)Ogretmen\n2-)Mimar\n3-)Yazilimci\n4-)Asci\n5-)Garson");
+            mesleksecenek = int.Parse(Console.ReadLine());
+
+            switch (mesleksecenek) {
+
+                case 1 :Console.WriteLine("Ortalama ucret : 13500-17000");break;
+                case 2 :Console.WriteLine("Ortalama ucret : 8000-25000");break;
+                case 3 :Console.WriteLine("Ortalama ucret : 12500-40000");break;
+                case 4 :Console.WriteLine("Ortalam ucret : 20000-30000");break;
+                case 5 :Console.WriteLine("Ortalama ucret : 5500-9000");break;
+                default :Console.WriteLine("Lutfen gecerli bir rakam giriniz.");
+                    goto a;
+
+            }
+        }
+    }
+}
