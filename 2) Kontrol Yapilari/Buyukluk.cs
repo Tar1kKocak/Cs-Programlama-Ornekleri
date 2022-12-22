e72f9682e74e38b29631c8af684cdba2a658fe5f
Tarık <116958073+Tar1kKocak@users.noreply.github.com>
2022-12-22 14:48:50 +0300

    Add files via upload

---
+++ 2) Kontrol Yapilari/Buyukluk.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace buyuk
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //Yazilimin Amaci: 4 tane sayıdan en büyük olanı bulan program.
+
+            int buyuk1, buyuk2, buyuk3, sayi1, sayi2, sayi3, sayi4;
+
+            Console.WriteLine("Dort adet sayi giriniz");
+            sayi1 = int.Parse(Console.ReadLine());
+            sayi2 = int.Parse(Console.ReadLine());
+            sayi3 = int.Parse(Console.ReadLine());
+            sayi4 = int.Parse(Console.ReadLine());
+
+            buyuk1 = Math.Max(sayi1, sayi2);
+            buyuk2 = Math.Max(sayi3, sayi4);
+
+            if (buyuk1 > buyuk2)
+            {
+                buyuk3 = buyuk1;
+            }
+            else
+            {
+                buyuk3 = buyuk2;
+            }
+            Console.WriteLine("En buyuk sayi:" + buyuk3);
+        }
+    }
+}
---
+++ 2) Kontrol Yapilari/Hesap Makinesi.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _33
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //Yazilimin Amaci: Hesap Makinesi
+
+            float sayi1, sayi2,sonuc;
+            char islem;
+
+            Console.WriteLine("1.sayiyi giriniz.");
+            sayi1 = float.Parse(Console.ReadLine());
+
+            Console.WriteLine("2.sayiyi giriniz.");
+            sayi2 = float.Parse(Console.ReadLine());
+
+            Console.WriteLine("Islem seciniz -> +, -, /, *, %");
+            islem = Convert.ToChar(Console.ReadLine());
+
+            if (islem == '+')
+            {
+                sonuc = sayi1 + sayi2;
+
+            }
+            else if (islem == '-')
+            {
+                sonuc = sayi1 - sayi2;
+            }
+            else if (islem == '/')
+            {
+                sonuc = sayi1 / sayi2;
+            }
+            else if (islem == '*')
+            {
+                sonuc = sayi1 * sayi2;
+            }
+            else
+            {
+                sonuc = sayi1 % sayi2;
+            }
+
+            Console.WriteLine("{0} {1} {2} = {3}", sayi1, islem, sayi2, sonuc);
+
+        }
+    }
+}
